ff1fa13a8e219ca04992defdf2b5789d504a6350
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2011-09-11 17:00:50 -0400

    first cut at library, util program

---
+++ FF.pm
@@ -0,0 +1,34 @@
+package FF;
+use strict;
+
+sub new {
+  my ($class, @x) = @_;
+  bless \@x => $class;
+}
+
+sub new_from_real {
+  my ($class, $x) = @_;
+  my $n = 2;
+  my @a;
+  while ($x) {
+    $x *= $n++;
+    push @a, int($x);
+    $x -= $a[-1];
+  }
+  return $class->new(@a);
+}
+
+sub new_from_rational {
+  my ($class, $n, $d) = @_;
+  my $c = 2;
+  my @a;
+  while ($n) {
+    $n *= $c++;
+    push @a, int($n/$d);
+    $n %= $d;
+  }
+  return $class->new(@a);
+}
+
+1;
+
